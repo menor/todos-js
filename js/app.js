05b83f26745b0f4bb75e32278cce5cb2685d6c3b
menor <josemenorgmail.com>
2014-04-08 11:11:54 -0700

    Add some MVC structure (based on Steven Harms implementation).

--- js/app.js
+++ js/app.js
@@ -1,14 +1,30 @@
+var Todos = {};
+
 $( document ).ready( function() {
+    Todos.todoTemplate = document.getElementById('#todo_template').children();
+    Todos.view = new Todos.View();
+    Todos.todos = new Todos.Store();
+    Todos.controller = new Todos.Controller();
+    Todos.view.controller = Todos.controller;
+    
     $( 'form' ).on( 'submit', function() {
         event.preventDefault();
         newTodoEvent( this.todo.value );
     });
 });
 
+
+// Controller
+function Todo() {
+}
+
+// View
+
+// Model
+function Store() {
+    this.store = {};
+}
+
 newTodoEvent = function( data ) {
     $( '.todos' ).append( data );
-};
-
-var template = function( data ) {
-    
 }
